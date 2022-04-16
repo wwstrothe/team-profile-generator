fc02ee30bb28688173fce44362b1869ccb52b355
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-04-16 16:06:38 -0700

    added intern test

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require("../lib/Intern");
+
+test("set school", () => {
+  const testValue = "South Harmon Institute of Technology";
+  const e = new Intern("William", 1, "william@test.com", testValue);
+  expect(e.school()).toBe(testValue);
+});
+
+test('getRole() should return "Intern"', () => {
+  const testValue = "Intern";
+  const e = new Intern("William", 1, "william@test.com");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("get GitHub username via getGitHub", () => {
+  const testValue = "bogusUser";
+  const e = new Intern("William", 1, "william@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
