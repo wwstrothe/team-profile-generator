d7aeb570ffb5d9c2e78016ebf64c8e70c8221257
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-04-16 19:04:46 -0700

    added inquirer questions rolls and push to dist/

--- index.js
+++ index.js
@@ -1,58 +1,172 @@
+const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
-const Manager = require('./lib/Manager')
 
-const inquire = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs')
 
 const generateSite = require('./src/generateSite');
-const Employee = require('./lib/Employee');
 
 team = [];
 
-const questions = {
-  message: "Please select the Team Members role:",
-  type: "list",
-  name: "role",
-  choices: ["Add Manager", "Add Engineer", "Add Intern", "Exit"],
-};
-
-const manager = {
-  message: "What is the Manager's office phone number",
-  type: "input",
-  name: "office",
-};
-
-const engineer = {
-  message: "What is the Engineer's GitHub username",
-  type: "input",
-  name: "github",
-};
-
-const intern = {
-  message: "What is the Intern's school name",
-  type: "input",
-  name: "school",
-};
-
-function start() {
-  employee = [
+const mQuestions = () => {
+  inquirer.prompt([
     {
-      message: "What is the Team Members name?",
-      type: "input",
-      name: "name",
+      type: 'input',
+      name: 'name',
+      message: "What is the team manager's name?",
     },
     {
-      message: "What is the Team Members ID number?",
-      type: "input",
-      name: "id",
+      type: 'input',
+      name: 'id',
+      message: "What is the team manager's id?",
     },
     {
-      message: "What is the Team Members email?",
-      type: "input",
-      name: "email",
+      type: 'input',
+      name: 'email',
+      message: "What is the team manager's email?",
     },
-  ];
-}
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: "What is the team manager's office number?",
+    },
+    {
+      type: 'list',
+      name: 'addMember',
+      message: 'What type of Team Member would you like to add?',
+      choices: ['Engineer', 'Intern', 'No more Team Members'],
+    }
+  ])
+  .then((mAnswers) => {
+    const manager = new Manager(
+      mAnswers.id, 
+      mAnswers.name, 
+      mAnswers.email, 
+      mAnswers.officeNumber
+      )
+    team.push(manager)
+    switch(mAnswers.addMember) {
+      case 'Engineer':
+        eQuestions();
+        break;
+      case 'Intern':
+        iQuestions();
+        break;
+      default: 
+      writeToFile('dist/index.html', generateSite(team))
+    }
+  });
+};
 
-start();
+const eQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the tEngineers's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the Engineers's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the Engineer's email?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the Engineers GitHub username?",
+      },
+      {
+        type: "list",
+        name: "addMember",
+        message: "What type of Team Member would you like to add?",
+        choices: ["Engineer", "Intern", "No more Team Members"],
+      },
+    ])
+    .then((eAnswers) => {
+      const engineer = new Engineer(
+        eAnswers.id,
+        eAnswers.name,
+        eAnswers.email,
+        eAnswers.github
+      );
+      team.push(engineer);
+      switch (eAnswers.addMember) {
+        case "Engineer":
+          eQuestions();
+          break;
+        case "Intern":
+          iQuestions();
+          break;
+        default:
+          writeToFile("dist/index.html", generateSite(team));
+      }
+    });
+};
+
+
+const iQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the Intern's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the Intern's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the Intern's email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is the Intern's school",
+      },
+      {
+        type: "list",
+        name: "addMember",
+        message: "What type of Team Member would you like to add?",
+        choices: ["Engineer", "Intern", "No more Team Members"],
+      },
+    ])
+    .then((iAnswers) => {
+      const intern = new Intern(
+        iAnswers.id,
+        iAnswers.name,
+        iAnswers.email,
+        iAnswers.github
+      );
+      team.push(intern);
+      switch (iAnswers.addMember) {
+        case "Engineer":
+          eQuestions();
+          break;
+        case "Intern":
+          iQuestions();
+          break;
+        default:
+          writeToFile("dist/index.html", generateSite(team));
+      }
+    });
+};
+
+mQuestions();
+
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, (err) => {
+        if(err) throw err;
+        console.log('file saved')
+    });
+};
